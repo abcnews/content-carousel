a562bbe6acba6bafcc8553a75220b549e0c74c42
Colin Gourlay <colin@colin-gourlay.com>
2021-11-12 12:10:26 +1000

    Include Image caption as title and (hidden, for now) figcaption

--- src/components/Image/Image.svelte
+++ src/components/Image/Image.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
   export let alt: string;
+  export let byline: string;
   export let src: string;
+  export let title: string;
 
   $: [, width] = src.match(/width=(\d+)/) || [];
   $: [, height] = src.match(/height=(\d+)/) || [];
   $: styleProps = `
     --cc-image-aspect-ratio: ${width} / ${height};
   `;
+  $: caption = `${title}${byline ? ` (${byline})` : ''}`;
 </script>
 
 <figure style={styleProps}>
-  <img {alt} {src} loading="lazy" />
+  <img {alt} {src} title={caption} loading="lazy" />
+  <figcaption>{caption}</figcaption>
 </figure>
 
 <style>
   figure {
+    position: relative;
     margin-left: calc(var(--cc-slide-padding) * -1);
     margin-left: calc(var(--cc-slide-padding) * -1);
     width: calc(100% + var(--cc-slide-padding) * 2);
@@ -37,4 +42,20 @@
     -webkit-user-select: none;
     -ms-user-select: none;
   }
+
+  figcaption {
+    display: none;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 8px 12px;
+    background-color: var(--cc-theme-caption-bg, var(--tint-4));
+    font-size: 0.75em;
+    line-height: 1.5;
+  }
+
+  /* figure:hover figcaption {
+    display: revert;
+  } */
 </style>
--- src/components/Image/utils.ts
+++ src/components/Image/utils.ts
@@ -1,14 +1,25 @@
 import { SvelteComponentTyped } from 'svelte';
 import Image from './Image.svelte';
 
+const EMPTY_NODE = document.createTextNode('');
+
+const getText = (el: Element, selector: string) =>
+  ((el.querySelector(selector) ?? EMPTY_NODE).textContent ?? '').trim();
+
 export const parse = (el: Element) => {
   const imgEl = el.querySelector('img') as HTMLImageElement;
+  const caption = getText(el, 'figcaption');
+  const cite = getText(el, 'figcaption cite');
+  const byline = getText(el, 'figcaption cite :first-child');
+  const title = caption.slice(0, caption.indexOf(cite));
 
   return {
     component: Image as unknown as SvelteComponentTyped,
     props: {
       alt: imgEl.getAttribute('alt'),
-      src: imgEl.getAttribute('data-src') || imgEl.getAttribute('src')
+      byline,
+      src: imgEl.getAttribute('data-src') || imgEl.getAttribute('src'),
+      title
     }
   };
 };
