7a9c90d2d447ed2859a5c9108ee7119961a4ea02
Colin Gourlay <colin@colin-gourlay.com>
2021-11-15 16:46:29 +1000

    Allow controls to be focusable, even when disabled (making them more accessible). Compress pips as needed

--- src/components/Carousel/Carousel.svelte
+++ src/components/Carousel/Carousel.svelte
@@ -21,6 +21,8 @@
   const SLIDES_GAP_PX = 8;
   const SLIDES_TRANSLATION_PX_S = 2500;
   const PRESENTATION_LAYER_SIDEBAR_ESTIMATED_WIDTH_PX = 300;
+  const PIPS_GAP_PX = 9;
+  const PIP_SIZE_PX = 8;
 </script>
 
 <script lang="ts">
@@ -59,8 +61,8 @@
     --cc-slides-transition-duration: ${slidesSwipeOffsetPx !== 0 ? 0 : baseWidthPx / SLIDES_TRANSLATION_PX_S}s;
     --cc-slide-padding-vertical: ${isInMultiColumnLayout ? 24 : 16}px;
     --cc-slide-padding-horizontal: ${isInMultiColumnLayout ? 32 : 20}px;
-    --cc-hint-mix-blend-mode: ${doesActiveSlideStartWithImage ? 'luminocity' : 'initial'};
     --cc-hint-color: hsl(0deg 0% ${doesActiveSlideStartWithImage ? 90 : 75}%);
+    --cc-hint-mix-blend-mode: ${doesActiveSlideStartWithImage ? 'luminocity' : 'initial'};
   `;
 </script>
 
@@ -96,18 +98,26 @@
       <button
         aria-controls={`${id}_slides`}
         aria-label="Previous slide"
-        disabled={slidesActiveIndex === 0}
+        aria-disabled={slidesActiveIndex === 0}
         on:click={() => goInDirection(-1)}
       >
         <svg role="presentation" viewBox="0 0 40 40">
           <polyline stroke="currentColor" stroke-width="2" fill="none" points="22.25 12.938 16 19.969 22.25 27" />
         </svg>
       </button>
-      <div aria-hidden="true">{`${slidesActiveIndex + 1} / ${slides.length}`}</div>
+      <div
+        class="pips"
+        class:crowded={slides.length > 10 && !isInMultiColumnLayout}
+        class:chockers={slides.length > 15 && !isInMultiColumnLayout}
+      >
+        {#each slides as _, index}
+          <div class="pip" class:current={slidesActiveIndex === index} />
+        {/each}
+      </div>
       <button
         aria-controls={`${id}_slides`}
         aria-label="Next slide"
-        disabled={slidesActiveIndex === slides.length - 1}
+        aria-disabled={slidesActiveIndex === slides.length - 1}
         on:click={() => goInDirection(1)}
       >
         <svg role="presentation" viewBox="0 0 40 40">
@@ -182,44 +192,34 @@
   }
 
   .controls {
+    position: relative;
+    margin-top: calc(var(--cc-slide-padding-vertical) / 3 * -2);
     display: flex;
-    gap: 8px;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     color: var(--cc-theme-controls-fg);
     font-size: 1rem;
     font-weight: bold;
   }
 
-  .controls div {
-    min-width: 3rem;
-    white-space: nowrap;
-    font-variant-numeric: tabular-nums;
-    letter-spacing: -0.02rem;
-    text-align: center;
-  }
-
   .controls button {
+    flex: 0 0 var(--cc-controls-height);
     display: block;
     padding: 0;
     border: 0;
     outline-offset: -2px;
     background-color: transparent;
     color: inherit;
+    pointer-events: none;
   }
 
-  .controls button[disabled] {
-    opacity: 0.2;
-  }
-
-  .controls button:not([disabled]):focus,
-  .controls button:not([disabled]):hover {
+  .controls button[aria-disabled='false']:focus {
     color: var(--cc-theme-controls-fg-focus);
     background-color: var(--cc-theme-controls-bg-focus, var(--tint-5));
-    cursor: pointer;
   }
 
   .controls svg {
+    opacity: 0;
     width: var(--cc-controls-height);
     height: var(--cc-controls-height);
     vertical-align: bottom;
@@ -227,6 +227,39 @@
 
   .controls :last-child > svg {
     transform: scaleX(-1);
+  }
+
+  .controls button[aria-disabled='true']:focus svg {
+    opacity: 0.2;
+  }
+
+  .controls button[aria-disabled='false']:focus svg {
+    opacity: 1;
+  }
+
+  .pips {
+    display: flex;
+    gap: 9px;
+  }
+
+  .pips.crowded {
+    gap: 6px;
+  }
+
+  .pips.chockers {
+    gap: 3px;
+  }
+
+  .pip {
+    border-radius: 50%;
+    width: 8px;
+    height: 8px;
+    background-color: var(--cc-theme-pip-background-color, var(--tint-1));
+    transition: background-color var(--cc-slides-transition-duration) ease-out;
+  }
+
+  .pip.current {
+    background-color: var(--cc-theme-pip-current-background-color, var(--tint-3));
   }
 
   .hint {
